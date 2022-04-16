7c5e8ca60e2f4d0f310f32194399980cb5dc1a86
joymohanta <joymohanta06@gmail.com>
2022-04-17 03:52:48 +0600

    active navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./Header.css";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-      <h2>This is header</h2>
+      <nav className="menu">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/checkout">Checkout</NavLink>
+        <NavLink to="/blogs">Blogs</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/login">Login</NavLink>
+      </nav>
     </header>
   );
 };
