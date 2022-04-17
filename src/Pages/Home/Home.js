e148e046e2380436f8194ba92e60aea35bc4d18e
joymohanta <joymohanta06@gmail.com>
2022-04-17 06:25:14 +0600

    carousel added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,33 @@
 import React from "react";
+import "./Home.css";
+import { Carousel } from "react-bootstrap";
 
 const Home = () => {
   return (
     <div>
-      <h2>This is Home</h2>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={"https://i.ibb.co/7KR7MbW/picthree.jpg"}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={"https://i.ibb.co/vqq2VyT/pictwo.jpg"}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={"https://i.ibb.co/jTbZB89/need.jpg"}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
